657e85b255994ae3804a3a6985af96064ba6d211
Mario Raeth <mario.raeth@ipp.mpg.de>
2023-07-13 17:21:43 +0200

    Code formatting

--- src/advectorSL.jl
+++ src/advectorSL.jl
@@ -1,47 +1,55 @@
 
 
-function advect1DFourier(data::Array{Float64,1}, shift, grid::Grid)
+function advect1DFourier!(data::AbstractArray{Float64,1}, shift, grid::Grid)
     sshift = 2pi * shift .* fftfreq(size(data)[1])
-    return   real(ifft(fft(data) .*exp.(-sshift.*im)))
+    data = real(ifft(fft(data) .* exp.(-sshift .* im)))
 end
 
 
-function advect1Dspline(data::Array{Float64,1}, shift, grid::Grid)
-    itp = interpolate([[data[end]];data; [data[begin]]], BSpline(Quadratic(Periodic(OnGrid()))))
-    sitp = scale(itp,0:length(data)+1)
-    return  sitp((1:length(data)).-shift)
+function advect1Dspline!(data::AbstractArray{Float64,1}, shift, grid::Grid)
+    itp = interpolate([[data[end]]; data; [data[begin]]], BSpline(Quadratic(Periodic(OnGrid()))))
+    sitp = scale(itp, 0:length(data)+1)
+    data = sitp((1:length(data)) .- shift)
 end
 
 
-function advectX!(f::DistributionGrid1d1v, grid::Grid)
+function advectX!(f::DistributionGrid1d1v, grid::Grid, advector=advect1DFourier!)
     for iv = 1:size(f.data)[2]
-        fshift =  grid.dt*grid.vaxes[1][iv]/grid.delta[1]
-        f.data[begin:end,iv] = advect1DFourier(f.data[begin:end,iv],fshift,grid)
-    end
-end
-
-function advectX!(f::DistributionGrid1d1v, grid::Grid, func)
-    for iv = 1:size(f.data)[2]
-        fshift =  grid.dt*grid.vaxes[1][iv]/grid.delta[1]
-        f.data[begin:end,iv] = func(f.data[begin:end,iv],fshift,grid)
+        fshift = grid.dt * grid.vaxes[1][iv] / grid.delta[1]
+        advector(view(f.data, :, iv), fshift, grid)
     end
 end
 
 
-function advectV!(f::DistributionGrid1d1v, grid::Grid, shiftArray::Array{Float64,1})
+function advectX!(f::DistributionGrid1d2v, grid::Grid, advector=advect1DFourier!)
+    for iv1 = 1:size(f.data)[2]
+        for iv2 = 1:size(f.data)[3]
+            fshift = grid.dt * grid.vaxes[1][iv] / grid.delta[1]
+            f.data[begin:end, iv1, iv2] = advector(f.data[begin:end, iv1, iv2], fshift, grid)
+        end
+    end
+end
+
+
+
+
+
+function advectV!(f::DistributionGrid1d1v, grid::Grid, shiftArray::Array{Float64,1}, advector=advect1DFourier!)
     for ix = 1:size(f.data)[1]
-        fshift = grid.dt*shiftArray[ix]/grid.delta[2]
-        f.data[ix,begin:end] = advect1DFourier(f.data[ix,begin:end],fshift,grid)
+        fshift = grid.dt * shiftArray[ix] / grid.delta[2]
+        advector(view(f.data, ix, :), fshift, grid)
     end
 end
 
-function advectV!(f::DistributionGrid1d1v, grid::Grid, shiftArray::Array{Float64,1}, funct)
-    for ix = 1:size(f.data)[1]
-        fshift = grid.dt*shiftArray[ix]/grid.delta[2]
-        f.data[ix,begin:end] = funct(f.data[ix,begin:end],fshift,grid)
+function advectV!(f::DistributionGrid1d2v, grid::Grid, shiftArray::Array{Float64,1}, advector=advect1DFourier!)
+    for iv1 = 1:size(f.data)[2]
+        advectV!(Distribution(f.data[:, iv1, :]), grid, shiftArray, advector)
     end
+
+    for iv2 = 1:size(f.data)[3]
+        advectV!(Distribution(f.data[:, :, iv2]))
+    end
+
 end
-
-
 
 
--- src/main.jl
+++ src/main.jl
@@ -9,20 +9,20 @@ nv = 64;
 
 epsilon = 0.1
 
-grid = vlasovSL.Grid([0.,-vmax],[Lx,vmax],[Lx/nx,2*vmax/nv],0.5,1);
+grid = vlasovSL.Grid([0.0, -vmax], [Lx, vmax], [Lx / nx, 2 * vmax / nv], 0.5, 1);
 
-f = vlasovSL.Distribution(grid,epsilon);
-fp = vlasovSL.Distribution(grid,epsilon,10000000);
+f = vlasovSL.Distribution(grid, epsilon);
+fp = vlasovSL.Distribution(grid, epsilon, 100000);
 
-sim = vlasovSL.Simulation(f,grid)
-simp =vlasovSL.Simulation(fp,grid);
+sim = vlasovSL.Simulation(f, grid)
+simp = vlasovSL.Simulation(fp, grid);
 
 
 function timeStep!(sim::vlasovSL.Simulation)
-    vlasovSL.advectX!(sim.f,sim.grid)
+    vlasovSL.advectX!(sim.f, sim.grid)
     sim.rho.data .= vlasovSL.compute_density(sim.f, sim.grid).data
     sim.e.data .= vlasovSL.compute_e(sim.rho, sim.grid).data
-    vlasovSL.advectV!(sim.f,sim.grid,sim.e.data[1])
+    vlasovSL.advectV!(sim.f, sim.grid, sim.e.data[1])
 end
 
 
@@ -39,19 +39,19 @@ println("Performance PIC")
 
 for i = vlasovSL.ProgressBar(1:2000)
     timeStep!(sim)
-    vlasovSL.diagnostics(sim,i)
+    vlasovSL.diagnostics(sim, i)
     timeStep!(simp)
-    vlasovSL.diagnostics(simp,i)
+    vlasovSL.diagnostics(simp, i)
 
-    p1 =vlasovSL.plotf(sim.f,sim.grid)
-    p2 =vlasovSL.plotf(simp.f,sim.grid)
-    p3 = vlasovSL.plot(simp.diag[1],simp.diag[2], yscale=:log10)
-    p3 = vlasovSL.plot!(sim.diag[1],sim.diag[2], yscale=:log10)
+    p1 = vlasovSL.plotf(sim.f, sim.grid)
+    p2 = vlasovSL.plotf(simp.f, sim.grid)
+    p3 = vlasovSL.plot(simp.diag[1], simp.diag[2], yscale=:log10)
+    p3 = vlasovSL.plot!(sim.diag[1], sim.diag[2], yscale=:log10)
 
     p4 = vlasovSL.plot(simp.rho.data)
     p4 = vlasovSL.plot!(sim.rho.data)
-    
-    vlasovSL.display(vlasovSL.plot(p1,p2,p3,p4, layout = (1,4), size = (1800,400)))
+
+    vlasovSL.display(vlasovSL.plot(p1, p2, p3, p4, layout=(1, 4), size=(1800, 400)))
 
 
 end
