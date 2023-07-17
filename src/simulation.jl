22aa0884d4a807dae48e9f22e9f981faa3c34c9c
Mario Raeth <mario.raeth@ipp.mpg.de>
2023-07-17 09:39:34 +0200

    Missing simulation.jl

--- src/simulation.jl
+++ src/simulation.jl
@@ -11,12 +11,11 @@ function Simulation(f::Distribution,grid::Grid)
 
     rho = vlasovSL.compute_density(f,grid);
     e = vlasovSL.compute_e(rho,grid);
-    return Simulation(f, rho, e,grid,[[],[]]);
+    return Simulation(f, rho, e,grid,[[],[],zeros(length(grid.itime), length(grid.xaxes[1]))]);
 end
-
-
 
 function diagnostics(sim::Simulation, iTime :: Int64)
    push!(sim.diag[1], iTime *sim.grid.dt)
    push!(sim.diag[2], sqrt(vlasovSL.mean(sim.rho.data.^2)))
+   sim.diag[3][iTime,:] = sim.rho.data
 end
