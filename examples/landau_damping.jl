6d17de31907ea4c02f776b67ff5343d5c472dd07
Mario <mraeth@NB-SD-01.ipp.mpg.de>
2023-09-05 10:54:18 +0200

    Create Example folder

--- examples/landau_damping.jl
+++ examples/landau_damping.jl
@@ -4,10 +4,10 @@ using vlasovSL, Plots, ProgressBars
 println("Num Threads = ", Threads.nthreads())
 
 Lx = 4pi;
-nx = 32;
+nx = 64;
 
 vmax = 4;
-nv = 32
+nv = 64
 
 nt = 5000
 dt = 0.05
@@ -15,9 +15,6 @@ dt = 0.05
 epsilon = 0.3
 
 grid = Grid([0.0, -vmax], [Lx, vmax], [Lx / nx, 2 * vmax / nv], dt, nt, 1, 0);
-grid = Grid([0.,-vmax,-vmax],[Lx,vmax,vmax],[Lx/nx,2*vmax/nv, 2*vmax/nv],dt,nt,1, 1;);
-gridPolar = Grid([0.,0,0],[Lx,vmax,2pi],[Lx/nx,vmax/nv, 2pi/nv],dt,nt,1, 1;type = vlasovSL.Polar);
-gridPolar = Grid([0.,-vmax,-vmax],[Lx,vmax,vmax],[Lx/nx,2*vmax/nv, 2*vmax/nv],dt,nt,1, 1;);
 
 
 initFuncx(x)= (1 .+ epsilon * cos(2pi/(grid.xaxes[1][end]+grid.delta[1])*x ))
@@ -25,10 +22,10 @@ initFuncv(v)= exp(-(v+1.5)^2 / 2) / sqrt(2*pi)+ exp(-(v-1.5)^2 / 2) / sqrt(2*pi)
 initFuncv(v) =  v^2*exp(-v^2 / 2) / sqrt(2*pi)
 
 f = Distribution(grid, epsilon);
-fp = Distribution(gridPolar, epsilon);
+fp = Distribution(grid, epsilon,1000000);
 
 sim = Simulation(f, grid)
-simp = Simulation(fp, gridPolar);
+simp = Simulation(fp, grid);
 
 
 function timeStep!(sim::Simulation)
@@ -53,7 +50,7 @@ println("Performance PIC")
 
 
 for grid.index[1]= ProgressBar(grid.itime)
-    gridPolar.index[1] = grid.index[1]
+    grid.index[1] = grid.index[1]
     timeStep!(sim)
     diagnostics(sim, grid.index[1])
     timeStep!(simp)
@@ -64,15 +61,13 @@ for grid.index[1]= ProgressBar(grid.itime)
         p1 = plotf(sim.f, sim.grid)
         p2 = plotf(simp.f, simp.grid)
 
-        p1v = plotfv(sim.f, sim.grid)
-        p2v = plotfv(simp.f, simp.grid)
         p3 = plot(sim.diag[1], sim.diag[2], yscale=:log10)
         p3 = plot!(simp.diag[1], simp.diag[2], yscale=:log10)
         
         p4 = plot(sim.e.data[1])
         p4 = plot!(simp.e.data[1])
         
-        display(plot(p1, p2, p3, p1v,p2v, p4, layout=(2,3), size=(1800, 500)))
+        display(plot(p1, p2, p3, p4, layout=(1,4), size=(1800, 500)))
     end 
 
 end
