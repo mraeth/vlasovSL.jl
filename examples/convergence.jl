0d1a7e4f8137e324dade62aff2ecd54acdd5d1cf
Mario Raeth <mario.raeth@ipp.mpg.de>
2023-11-17 20:30:28 +0100

    Fixed convergence test

--- examples/convergence.jl
+++ examples/convergence.jl
@@ -2,11 +2,11 @@ using vlasovSL, Plots, ProgressBars, DSP, FFTW, SpecialFunctions
 
 println("Num Threads = ", Threads.nthreads())
 
-Lx = 8pi;
+Lx = 20/3 * π;
 
 vmax = 4
-nv = 32
-nx = 32
+nv = 33
+nx = 64
 B=0
 
 epsilon = 0.025
@@ -22,6 +22,9 @@ end
 function computeXadvection(sim::Simulation, dt::Float64)
     advectX!(sim.f, sim.grid,dt)
 end
+
+
+
 
 function timeStep!(sim::Simulation, dt::Float64)
     computeXadvection(sim, dt)
@@ -52,24 +55,24 @@ function timeStepStrang2!(sim::Simulation, dt::Float64)
 end
 
 function timeStepStrang3!(sim::Simulation, dt::Float64)
-    if sim.grid.index[1]==1
-        computeVadvection(sim, dt/2)
-        println("First half timestep in v")
-    end
+    compute_density!(sim.rho, sim.f, sim.grid)
+    adiabatic!(sim.phi, sim.rho, sim.grid)
+    compute_e!(sim.e, sim.phi, sim.grid)
+    # sim.e.data[1].=0.01
+
+
+    advectV!(sim.f, sim.grid, dt/2, sim.e)
     computeXadvection(sim,dt)
-    if sim.grid.index[1]==sim.grid.itime[end]
-        computeVadvection(sim, dt/2)
-        println("Last half timestep in v")
-    else
-        computeVadvection(sim,dt/2)
-        sim.grid.curr_time[1]+=dt
-        computeVadvection(sim,dt/2)
-    end
+    advectV!(sim.f, sim.grid, dt/2, sim.e)
+    
+    sim.grid.curr_time[1]+=dt
 end
 
 
 
+
 results = []
+Tmax = 4
 deltaT = [[1/(10*2^i) for i =1:4];1/(10*2^7)]
 
 for tS in [timeStep!,timeStepStrang1!,timeStepStrang2!,timeStepStrang3!]
@@ -78,7 +81,7 @@ for tS in [timeStep!,timeStepStrang1!,timeStepStrang2!,timeStepStrang3!]
 
         println(tS, ", ΔT=",dt)
 
-        nt = convert(Int,1/dt)
+        nt = convert(Int,Tmax/dt)
         grid = Grid([0.,-vmax,-vmax],[Lx,vmax,vmax],[Lx/nx,2*vmax/nv, 2*vmax/nv],dt,nt,1, B;);
 
         f = Distribution(grid, epsilon;);
@@ -86,9 +89,7 @@ for tS in [timeStep!,timeStepStrang1!,timeStepStrang2!,timeStepStrang3!]
         sim = Simulation(f, grid)
 
         for sim.grid.index[1]= sim.grid.itime
-            # println(sim.grid.index[1])
             tS(sim, sim.grid.dt)
-            # diagnostics(sim, grid.index[1])
         end
         push!(result, sim.rho.data)
     end
